000defe13feb07815e89d05cf2a8c063071cb85c
Felix Bolte <bolte.felix@gmail.com>
2016-03-02 12:46:43 +0100

    cleanup

---
+++ template/README.md
@@ -0,0 +1,5 @@
+# qual-o-mat-data / jahr / ort
+
+## Datenquelle
+
+Die Daten stammen aus [diesem](link) PDF-Dokument.
--- tools/analyze_pdf.sh
+++ tools/analyze_pdf.sh
@@ -7,10 +7,10 @@
 # neutral = 2
 
 # checks
-[ ! -f "$1" ] && echo "First parameter must be an image!" >&2 && exit 1
+[ ! -f "$1" ] && echo "ERROR: First parameter must be an image. Abort!" >&2 && exit 1
 
 SCRIPT_DIR="$(cd "`dirname "$0"`" && pwd)"
-[ -z "$SCRIPT_DIR" ] && echo "Script directory could not be detected!" >&2 && exit 1
+[ -z "$SCRIPT_DIR" ] && echo "ERROR: Script directory could not be detected. Abort!" >&2 && exit 1
 
 # execute final command
 gocr -d 0 -l 200 -p "$SCRIPT_DIR/db/" -m 258 -a 40 -u 2 -i "$1" | grep -v ^$ | tr -d " \t"
