839803dea70deb43eb926d78d5fbd96db04266d6
Felix Bolte <bolte.felix@gmail.com>
2017-09-11 14:58:21 +0200

    add some more party comments

--- tools/parse_comments.py
+++ tools/parse_comments.py
@@ -1,59 +1,78 @@
 #!/usr/bin/env python3
 
+import sys
 import json
 import bleach
 from io import StringIO
 from lxml import etree
 from lxml import html
 
+STATEMENTS = 38
+
+# check for needed arguments
+if len(sys.argv) < 3:
+    print('Missing arguments: [script] opinion.json result.html')
+    sys.exit(1)
+
 # load "old" opinion json file into an object
-with open('../2017/deutschland/opinion.json', 'r') as json_file:
+with open(sys.argv[1], 'r') as json_file:
      opinions = json.load(json_file)
 
-def fill_opinion(html_file):
-    with open(html_file, 'r') as f:
-        filecontent = f.read()
-        
-        # remove newlines and tabs
-        filecontent = filecontent.replace('\n', ' ')
-        filecontent = filecontent.replace('\t', ' ')
-        
-        # get html structure for using xpath
-        tree = html.parse(StringIO(filecontent))
-        # get all divs with party comment to statement
-        divs = tree.xpath('//ul[@class="wom_votum_list wom_on"]//div')
-        
-        # manual check if comments match party*statements
-        print(len(divs))
-        
-        for div in divs:
-            # get div class and thus party id and statement id
-            divid = div.get('id')
-            party = int(divid.split('-')[2].split('partei')[1])
-            statement = int(divid.split('-')[3]) - 1
-            # just strip out text without html elements
-            blockquote = div.xpath('./blockquote')
-            text = etree.tostring(blockquote[0]).decode()
-            cleantext = bleach.clean(text, tags=[], attributes={}, styles=[], strip=True)
-            # remove multiple whitespaces
-            cleantext = ' '.join(cleantext.split())
-            # replace double quotes with single quote
-            cleantext = cleantext.replace('"', '\'')
-            # replace german quotes with common quotes
-            cleantext = cleantext.replace('„', '"')
-            cleantext = cleantext.replace('”', '"')
-            
-            # match opinion and add comment key
-            for opinion in opinions:
-                if opinion['party'] == party and opinion['statement'] == statement:
-                    opinions[opinion['id']]['opinion'] = opinion['id']
-                    opinions[opinion['id']]['text'] = cleantext
+# load html result page (put all htmls in one file)
+with open(sys.argv[2], 'r') as f:
+    filecontent = f.read()
 
-# read all result pages
-fill_opinion('b2017_1.html')
-fill_opinion('b2017_2.html')
-fill_opinion('b2017_3.html')
-fill_opinion('b2017_4.html')
+# remove newlines and tabs
+filecontent = filecontent.replace('\n', ' ')
+filecontent = filecontent.replace('\t', ' ')
+
+# get html structure for using xpath
+tree = html.parse(StringIO(filecontent))
+# get all elements with party comment to statement
+comments = tree.xpath('//ul[@class="wom_votum_list wom_on"]//div')
+if len(comments) == 0:
+    # different format in the past
+    comments = tree.xpath('//ul[@class="votum_list on"]//div')
+
+# check consistency and give some output for the user
+print(str(len(comments)) + ' comments found.')
+print(str(len(opinions)) + ' opinions found.')
+if len(comments) != len(opinions):
+    print('Something seems missing. Exit.')
+    sys.exit(1)
+
+comment_cnt = 0
+for comment in comments:
+    # calculate party id and statement id using current comment
+    party = comment_cnt//STATEMENTS
+    statement = comment_cnt%STATEMENTS
+    # find correct part in new format
+    quote = comment.xpath('./blockquote')
+    if len(quote) == 0:
+        text = etree.tostring(comment).decode()
+    else:
+        text = etree.tostring(quote[0]).decode()
+    # just strip out text without html elements (+ convert html escapes to normal text)
+    cleantext = bleach.clean(text, tags=[], attributes={}, styles=[], strip=True)
+    # remove multiple whitespaces
+    cleantext = ' '.join(cleantext.split())
+    # replace double quotes with single quote
+    cleantext = cleantext.replace('"', '\'')
+    # replace german quotes with common quotes
+    cleantext = cleantext.replace('„', '"')
+    cleantext = cleantext.replace('”', '"')
+    # concert html escape for & to normal &
+    cleantext = cleantext.replace('&amp;', '&')
+    # remove this part for old format
+    cleantext = cleantext.replace('Begründung der Partei:', '')
+    
+    # match opinion and add comment key
+    for opinion in opinions:
+        if opinion['party'] == party and opinion['statement'] == statement:
+            opinions[opinion['id']]['opinion'] = opinion['id']
+            opinions[opinion['id']]['text'] = cleantext
+    
+    comment_cnt += 1
 
 # remove keys we dont need anymore for comment.json
 for opinion in opinions:
